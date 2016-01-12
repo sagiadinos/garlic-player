2f81237cba69b09f797207ee990bc6564e4dfde2
Nikolaos Sagiadinos <ns@smil-control.com>
2016-01-13 00:58:15 +0100

    forgotten agpl header

--- src/smilparser/timings/clock_value.h
+++ src/smilparser/timings/clock_value.h
@@ -1,3 +1,21 @@
+/*************************************************************************************
+    garlic-player: SMIL Player for Digital Signage
+    Copyright (C) 2016 Nikolaos Saghiadinos <ns@smil-.control.com>
+    This file is part of the garlic-player source code
+
+    This program is free software: you can redistribute it and/or  modify
+    it under the terms of the GNU Affero General Public License, version 3,
+    as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************************/
+
 #ifndef TCLOCKVALUE_H
 #define TCLOCKVALUE_H
 #include <QObject>
