88d7c4dcf34a2775ff35b29778b9368cf619ad85
Nikolaos Sagiadinos <sagiadinos@users.noreply.github.com>
2016-09-16 01:21:05 +0200

    adjust download tests to new subdomain

--- tests/downloader/tst_downloader.cpp
+++ tests/downloader/tst_downloader.cpp
@@ -41,7 +41,7 @@ void TestTDownloader::testAgentString()
     QString user_agent        = "GAPI/1.0 (UUID:f9d65c88-e4cd-43b4-89eb-5c338e54bcae; NAME:TestTDownload) xxxxxx-xx/x.x.x (MODEL:GARLIC)";
     TDownloader *MyDownloader = new TDownloader(user_agent);
     QString file_name         = "./agent.txt";
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=show_agent_string");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=show_agent_string");
     QSignalSpy spy(MyDownloader, SIGNAL(downloadSucceed(QString)));
     while (spy.count() == 0)
         QTest::qWait(200);
@@ -60,7 +60,7 @@ void TestTDownloader::testWithoutAgentString()
     QString user_agent        = "";
     TDownloader *MyDownloader = new TDownloader(user_agent);
     QString file_name         = "./agent.txt";
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=show_agent_string");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=show_agent_string");
 
     QFile file(file_name);
     QVERIFY(!file.exists());
@@ -71,7 +71,7 @@ void TestTDownloader::testDownloadSmil()
     QString user_agent        = "GAPI/1.0 (UUID:f9d65c88-e4cd-43b4-89eb-5c338e54bcae; NAME:TestTDownload) xxxxxx-xx/x.x.x (MODEL:GARLIC)";
     TDownloader *MyDownloader = new TDownloader(user_agent);
     QString file_name         = "./test.smil";
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_smil_new");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=get_smil_new");
     QSignalSpy spy1(MyDownloader, SIGNAL(downloadSucceed(QString)));
     while (spy1.count() == 0)
         QTest::qWait(200);
@@ -79,7 +79,7 @@ void TestTDownloader::testDownloadSmil()
     QVERIFY(file.exists());
     QFileInfo fi(file);
     QDateTime last_modified = fi.lastModified();
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_smil_new");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=get_smil_new");
     QSignalSpy spy2(MyDownloader, SIGNAL(noModified(QString)));
     while (spy2.count() == 0)
         QTest::qWait(200);
@@ -88,7 +88,7 @@ void TestTDownloader::testDownloadSmil()
 
     QTest::qWait(500); // to write file with new date
 
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_smil_cached");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=get_smil_cached");
     QSignalSpy spy3(MyDownloader, SIGNAL(noModified(QString)));
     while (spy3.count() == 0)
         QTest::qWait(200);
@@ -96,7 +96,7 @@ void TestTDownloader::testDownloadSmil()
     QCOMPARE(last_modified, fi3.lastModified());
 
 
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_smil_updated");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=get_smil_updated");
     QSignalSpy spy4(MyDownloader, SIGNAL(downloadSucceed(QString)));
     while (spy4.count() == 0)
         QTest::qWait(200);
@@ -110,7 +110,7 @@ void TestTDownloader::testDownloadMedia()
     QString user_agent        = "GAPI/1.0 (UUID:f9d65c88-e4cd-43b4-89eb-5c338e54bcae; NAME:TestTDownload) xxxxxx-xx/x.x.x (MODEL:GARLIC)";
     TDownloader *MyDownloader = new TDownloader(user_agent);
     QString file_name         = "./test.jpg";
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_media_new");
+    MyDownloader->checkFiles(file_name, "http:/admin.smil-admin.com/garlic/test.php?action=get_media_new");
     QSignalSpy spy1(MyDownloader, SIGNAL(downloadSucceed(QString)));
     while (spy1.count() == 0)
         QTest::qWait(200);
@@ -119,7 +119,7 @@ void TestTDownloader::testDownloadMedia()
     QFileInfo fi(file);
     QDateTime last_modified = fi.lastModified();
 
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_media_new");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=get_media_new");
     QSignalSpy spy2(MyDownloader, SIGNAL(noModified(QString)));
     while (spy2.count() == 0)
         QTest::qWait(200);
@@ -127,14 +127,14 @@ void TestTDownloader::testDownloadMedia()
     QCOMPARE(last_modified, fi2.lastModified());
     QTest::qWait(500); // to write file with new date
 
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_media_cached");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=get_media_cached");
     QSignalSpy spy3(MyDownloader, SIGNAL(noModified(QString)));
     while (spy3.count() == 0)
         QTest::qWait(200);
     QFileInfo fi3(file);
     QCOMPARE(last_modified, fi3.lastModified());
 
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/test.php?action=get_media_updated");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/test.php?action=get_media_updated");
     QSignalSpy spy4(MyDownloader, SIGNAL(downloadSucceed(QString)));
     while (spy4.count() == 0)
         QTest::qWait(200);
@@ -148,7 +148,7 @@ void TestTDownloader::testDownloadMediaDirect()
     QString user_agent        = "GAPI/1.0 (UUID:f9d65c88-e4cd-43b4-89eb-5c338e54bcae; NAME:TestTDownload) xxxxxx-xx/x.x.x (MODEL:GARLIC)";
     TDownloader *MyDownloader = new TDownloader(user_agent);
     QString file_name         = "./server.jpg";
-    MyDownloader->checkFiles(file_name, "http://smil-admin.com/garlic/server.jpg");
+    MyDownloader->checkFiles(file_name, "http://admin.smil-admin.com/garlic/server.jpg");
     QSignalSpy spy1(MyDownloader, SIGNAL(downloadSucceed(QString)));
     while (spy1.count() == 0)
         QTest::qWait(200);
