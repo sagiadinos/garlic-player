26501ac594f5f8f9ff49948ab9b9368decfea47e
sagiadinos <ns@smil-control.com>
2017-03-30 14:55:06 +0200

    updated deployment for win

--- deployWindows.bat
+++ deployWindows.bat
@@ -1,7 +1,15 @@
 @echo off
 rem execute this script in a VS Native CMD
 
-set QT_PATH=
+rem put all files needed and structured in %DEPLOYMENT_HELPER_PATH%
+set DEPLOYMENT_HELPER_PATH=C:\garlic-deployment
+if "%DEPLOYMENT_HELPER_PATH%" == "" (
+	echo on
+	echo Error: Set the correct paths for DEPLOYMENT_HELPER_PATH 
+	goto END
+)
+
+set QT_PATH=C:\Qt
 set QT_PATH_RUNTIME=%QT_PATH%\5.8\msvc2015_64
 if "%QT_PATH%" == "" (
 	echo on
@@ -15,16 +23,37 @@ cd build
 %QT_PATH_RUNTIME%\bin\qmake.exe ..\src\complete.pro
 %QT_PATH%\Tools\QtCreator\bin\jom.exe
 
-rem put all files needed and structured in C:\deployment\*
+%QT_PATH_RUNTIME%\bin\windeployqt.exe -opengl -webengine bin\garlic-player.exe
+missing Qt-Dlls which windeplayqt is not able to copy
+copy /Y  %QT_PATH_RUNTIME%\bin\Qt5WebSockets.dll bin\*
+copy /Y  %QT_PATH_RUNTIME%\bin\Qt5WebView.dll bin\*
+copy /Y  %QT_PATH_RUNTIME%\bin\Qt5XmlPatterns.dll bin\*
 
-xcopy C:\deployment\bin\* ..\deployment\win\packages\com.sagiadinos.garlic.x64\data\bin\* /sy
-copy ..\bin\garlic-player.exe ..\deployment\win\packages\com.sagiadinos.garlic.x64\data\bin\garlic-player.exe
-copy C:\deployment\dx_sdk2010_xaudio2_redist\* ..\deployment\win\packages\com.microsoft.directx.xaudio2\data\*
-copy C:\deployment\vc_redist.x64.exe ..\deployment\win\packages\com.microsoft.vcredist.x64\data\*
+copy QtAV-Dlls
+copy /Y  %QT_PATH_RUNTIME%\bin\QtAV1.dll bin\*
+copy /Y  %QT_PATH_RUNTIME%\bin\QtAVWidgets1.dll bin\*
 
-%QT_PATH%\Tools\QtInstallerFramework\2.0\bin\binarycreator -c  ..\deployment\win\config\config.xml -p  ..\deployment\win\packages ..\garlic-installer.exe
+copy ffmpeg dlls
+copy /Y %QT_PATH_RUNTIME%\bin\av*.dll bin\*
+copy /Y  %QT_PATH_RUNTIME%\bin\sw*.dll bin\*
+copy /Y  %DEPLOYMENT_HELPER_PATH%\libeay32.dll bin\*
+copy /Y  %DEPLOYMENT_HELPER_PATH%\ssleay32.dll bin\*
+
+if not exist deployment mkdir deployment
+xcopy ..\deployment\win\* deployment\* /sy
+
+if not exist deployment\packages\com.microsoft.vcredist.x64\data mkdir deployment\packages\com.microsoft.vcredist.x64\data
+move bin\vcredist_x64.exe deployment\packages\com.microsoft.vcredist.x64\data
+
+if not exist deployment\packages\com.microsoft.directx.xaudio2\data mkdir deployment\packages\com.microsoft.directx.xaudio2\data
+copy /Y %DEPLOYMENT_HELPER_PATH%\dx_sdk2010_xaudio2_redist\* deployment\packages\com.microsoft.directx.xaudio2\data\*
+
+if not exist deployment\packages\com.sagiadinos.garlic.x64\data\bin mkdir deployment\packages\com.sagiadinos.garlic.x64\data\bin
+xcopy bin\* deployment\packages\com.sagiadinos.garlic.x64\data\bin\* /sy
 
 
-cd ..
+%QT_PATH%\Tools\QtInstallerFramework\2.0\bin\binarycreator -c  deployment\config\config.xml -p  deployment\packages garlic-installer.exe
 
 :END
+cd ..
+
